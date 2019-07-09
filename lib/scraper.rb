b4550057e0b0e8b9c21acd4025bc2dc2fe7f846e
Tom White <eugene.white@gmail.com>
2019-07-09 23:44:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,16 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = File.read(index_url)
-    flatiron = Nokogiri::HTML(open(html))
-    binding.pry
+    doc = Nokogiri::HTML(html)
+    students = []
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        profile_url = "#{student.attr("href")}"
+        students << {name: name, location: location, profile_url: profile_url}
+      end
+      students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -16,6 +24,6 @@ class Scraper
 
 end
 
-# name: flatiron.css("h4.student-name").text
-# location: flatiron.css("p.student-location").text
-# profile_url: flatiron.css("div.roster-cards-container div.student-card a").attr("href").value
+# name: doc.css("h4.student-name").text
+# location: doc.css("p.student-location").text
+# profile_url: doc.css("div.roster-cards-container div.student-card a").attr("href").value
