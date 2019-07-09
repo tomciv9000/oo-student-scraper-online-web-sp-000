3bfee0fe8e497bccb5b8d1227be3e3ae36316764
Tom White <eugene.white@gmail.com>
2019-07-09 23:15:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = File.read(index_url)
-    flatiron = Nokogiri::HTML(html)
+    flatiron = Nokogiri::HTML(open(html))
     binding.pry
   end
 
@@ -17,4 +17,5 @@ class Scraper
 end
 
 # name: flatiron.css("h4.student-name").text
-
+# location: flatiron.css("p.student-location").text
+# profile_url: flatiron.css("div.roster-cards-container div.student-card a").attr("href").value
