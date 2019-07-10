bdda783fd6a39b86e332ddcde22ef2d8b18b1806
Tom White <eugene.white@gmail.com>
2019-07-10 03:13:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,8 @@ class Student
   def initialize(student_hash)
     student_hash.each do |key, value|
       self.send("#{key}=", value)
-      @@all << self
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
